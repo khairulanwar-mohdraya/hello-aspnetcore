b5014b72deece68f04f7d4476ed2f308619763e0
khairulanwar-mohdraya <khairulanwar.mohdraya@gmail.com>
2022-12-18 13:15:13 +0800

    test commit from laptop

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Kewl", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezed", "Braced", "Chilled", "Kool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
